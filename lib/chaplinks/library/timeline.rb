af1808cdbfd5f9c20211404dc288fda6604d93ea
Roman Gnatyuk <fozzy@hackers.net.ua>
2013-08-21 12:35:10 +0300

    Autoload

--- lib/chaplinks/library/timeline.rb
+++ lib/chaplinks/library/timeline.rb
@@ -3,7 +3,8 @@ require "chaplinks/library/timeline/version"
 module Chaplinks
   module Library
     module Timeline
-      # Your code goes here...
+      class Engine < ::Rails::Engine
+      end
     end
   end
 end
