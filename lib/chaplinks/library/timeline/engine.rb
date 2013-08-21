0c6259b5d9c453c75e3e43873780e7c7752cfa40
Roman Gnatyuk <fozzy@hackers.net.ua>
2013-08-21 12:30:28 +0300

    Autoload

---
+++ lib/chaplinks/library/timeline/engine.rb
@@ -0,0 +1,10 @@
+require "chaplinks-library-timeline/version"
+
+module Chaplinks
+  module Library
+    module Timeline
+      class Engine < ::Rails::Engine
+      end
+    end
+  end
+end
---
+++ vendor/assets/javascripts/chaplinks-library/timeline/index.js
@@ -0,0 +1,2 @@
+//= require timeline
+//= require timeline-locales
